962a1488cb8e40458fbac65675b6216cb55b145a
jc.ligot <jc.ligot@gmail.com>
2023-05-08 11:53:02 +0200

    simple form

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { w2form, query } from 'w2ui/w2ui-2.0.js';
 @Component({
   selector: 'app-form',
   templateUrl: './form.component.html',
@@ -9,5 +9,35 @@ import { Component, OnInit } from '@angular/core';
 export class FormComponent  implements OnInit {
   ngOnInit(): void {
     console.log('form');
+
+    // this function triggers on document.ready
+    let form = new w2form({
+        box: '#form',
+        name: 'form',
+        record: {
+            first_name: 'John',
+            last_name: 'Doe'
+        },
+        // formURL: 'data/form.html',    // you can load form from extenral file
+        formHTML: query('#form').html(), // or you can use form that is already in HTML
+        fields: [
+            { field: 'first_name', type: 'text', required: true },
+            { field: 'last_name',  type: 'text', required: true },
+            { field: 'comments',   type: 'text'}
+        ],
+        actions: {
+            reset() {
+                this.clear()
+            },
+            save() {
+                this.save()
+            }
+        }
+    })
+
+
+
+
+
   }
 }
